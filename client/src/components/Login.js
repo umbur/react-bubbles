5ed266733e370cb6d16c7a5cfcad2c698bacfb1a
umbur <you@example.com>
2019-08-23 10:30:27 -0600

    created login page to retrieve a token; and protected route

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,60 @@
-import React from "react";
+import React from 'react';
+import axios from 'axios';
+import { Route, Redirect } from 'react-router';
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
+class Login extends React.Component {
+  state = {
+    credentials: {
+      username: '',
+      password: ''
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  login = e => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:5000/api/login', this.state.credentials)
+      .then(res => {
+        localStorage.setItem('token', res.data.payload);
+        console.log(res)
+        
+      })
+      .catch(err => console.log(err.response));
+  };
+
+  render() {
+    return (
+      <div>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+        <form onSubmit={this.login}>
+          <input
+            type="text"
+            name="username"
+            placeholder='Username'
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder='Password'
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
+          />
+          <button>Log in</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Login;
